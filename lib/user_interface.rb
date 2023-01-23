e2232715c7072f5960a5ad62507a869f62023add
Joe Burgess <josephburgess@gmail.com>
2023-01-23 07:31:42 +0000

    adding user interface

--- lib/user_interface.rb
+++ lib/user_interface.rb
@@ -9,4 +9,57 @@ class UserInterface
     @game = game
     @frame_count = 1
   end
+
+  def run
+    show 'Welcome to your bowling scorecard!'
+    until @frame_count == 10
+      show "You are on frame #{@frame_count}."
+      input_rolls
+    end
+  end
+
+  private
+
+  def show(message)
+    @io.puts(message)
+  end
+
+  def prompt(message)
+    @io.puts(message)
+    @io.gets.chomp.to_i
+  end
+
+  def input_rolls
+    roll_1 = prompt 'Input your roll:'
+    if roll_1 > 10
+      show 'Invalid input'
+      input_rolls
+    elsif roll_1 == 10
+      show 'You got a strike!'
+      roll_2 = 0
+      record_frame(roll_1, roll_2)
+    else
+      input_second_roll(roll_1)
+    end
+  end
+
+  def input_second_roll(roll_1)
+    roll_2 = prompt "You got #{roll_1}, please input second roll:"
+    if roll_1 + roll_2 > 10
+      show 'Invalid input'
+      input_second_roll
+    elsif roll_1 + roll_2 == 10
+      show 'You got a spare!'
+      record_frame(roll_1, roll_2)
+    else
+      show "You got #{roll_1 + roll_2}"
+      record_frame(roll_1, roll_2)
+    end
+  end
+
+  def record_frame(roll_1, roll_2)
+    frame = Frame.new(roll_1, roll_2)
+    @game.add(frame)
+    @frame_count += 1
+  end
 end
--- spec/user_interface_spec.rb
+++ spec/user_interface_spec.rb
@@ -1,1 +1,23 @@
-
+require_relative '../lib/user_interface'
+RSpec.describe UserInterface do
+  describe 'bowling ' do
+    it 'asks the user to input their rolls' do
+      io = double(:io)
+      game = double(:game, scorecard: [])
+      frame = Frame.new(10, 0)
+      interface = UserInterface.new(io, game)
+      expect(io).to receive(:puts).with('Welcome to your bowling scorecard!')
+      expect(io).to receive(:puts).with('You are on frame 1.')
+      expect(io).to receive(:puts).with('Input your roll:')
+      expect(io).to receive(:gets).and_return('10')
+      expect(io).to receive(:puts).with('You got a strike!')
+      expect(game).to receive(:add).with(an_instance_of(Frame))
+      expect(game).to receive(:calculate_total_score).and_return('10')
+      expect(io).to receive(:puts).with('Your current score is 10')
+      expect(io).to receive(:puts).with('You are on frame 2.')
+      expect(io).to receive(:puts).with('Input your roll:')
+      expect(io).to receive(:gets).and_return('0')
+      interface.run
+    end
+  end
+end
